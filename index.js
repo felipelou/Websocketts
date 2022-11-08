de8f179736f593d5b040480f7781f051f91bed33
Felipe Alejandro Rosas Mendoza <felipelou123499@gmail.com>
2022-11-07 18:25:03 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Archivos
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
